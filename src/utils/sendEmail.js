5f03b2349935a48235200daca360f130562d913c
darunbjork <darunbjork@gmail.com>
2025-09-11 22:23:22 +0200

    feat: Refactor email service to use SendGrid REST API
    Refactored `src/utils/sendEmail.js` to use the `@sendgrid/mail` package and SendGrid's REST API for sending emails. This addresses the persistent 500 Internal Server Error in the "Forgot Password" functionality, which was caused by authentication issues when using SendGrid's SMTP interface with an API key.
    
    The change improves the reliability and maintainability of the email service.

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,24 +1,15 @@
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: "SendGrid",
-    auth: {
-      user: "apikey", // This is literally the string "apikey"
-      pass: process.env.SENDGRID_API_KEY,
-    },
-  });
-
-  const message = {
-    from: process.env.FROM_EMAIL, // Use FROM_EMAIL from .env, which should be verified
+  const msg = {
     to: options.email,
+    from: process.env.FROM_EMAIL,
     subject: options.subject,
-    text: options.message,
+    html: options.message,
   };
-
-  const info = await transporter.sendMail(message);
-
-  console.log('Email sent: %s', info.messageId);
+  
+  return sgMail.send(msg);
 };
 
 module.exports = sendEmail;
