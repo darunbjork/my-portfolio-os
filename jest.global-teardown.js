c0a2d6129cb969af2d1ad36a17eb380e1edf6230
darunbjork <darunbjork@gmail.com>
2026-05-01 21:55:00 +0200

    fix: Correct cache import path in jest.global-teardown.js

--- jest.global-teardown.js
+++ jest.global-teardown.js
@@ -1,8 +1,12 @@
-// jest.global-teardown.js
+const cache = require('./src/utils/cache');
+
 module.exports = async () => {
   // Stop the MongoDB Memory Server
   if (global.__MONGO_SERVER__) {
     await global.__MONGO_SERVER__.stop();
     console.log('MongoDB Memory Server stopped.');
   }
+
+  await cache.disconnect();
+  console.log('Redis client disconnected.');
 };
